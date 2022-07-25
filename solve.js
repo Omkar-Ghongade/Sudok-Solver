7506b789327d4f1d9f2aad516d9968c8faac8c92
Omkar Subhash Ghongade <88375748+Omkar-Ghongade@users.noreply.github.com>
2022-07-25 21:38:39 +0530

    Add files via upload

---
+++ solve.js
@@ -0,0 +1,70 @@
+function check(sudoku,r,c,val) {
+    for (var i = 0; i < 9; i++)
+        if (sudoku[i][c] == val)
+            return false
+    
+    for (var i = 0; i < 9; i++)
+        if (sudoku[r][i] == val)
+            return false
+    
+    var x = r - r % 3, y = c - c % 3
+    
+    for (var i = 0; i < 3; i++){
+        for (var j = 0; j < 3; j++){
+            if (sudoku[x + i][y + j] == val)
+                return false
+        }
+    }
+    
+    return true
+}
+
+
+function solve(sudoku) {
+    for (var i = 0; i < 9; i++){
+        for (var j = 0; j < 9; j++){
+            const I = String.fromCharCode(48 + i)
+            const J = String.fromCharCode(48 + j)
+            let s=I+J
+            if (sudoku[i][j] == '') {
+                for (var K = 1; K <= 9; K++)
+                {
+                    const k=String.fromCharCode(48+K)
+                    if (check(sudoku,i,j,k)) {
+                        sudoku[i][j] = String.fromCharCode(48 + K   )
+                        document.getElementById(s).value = sudoku[i][j]
+
+                        if (solve(sudoku) == true)
+                            return true
+                        else
+                            sudoku[i][j]=''
+                    }
+                }
+                return false;
+            }
+        }
+    }
+    return true
+}
+
+
+function create() {
+    let sudoku = new Array(9);
+
+    for (var i = 0; i < 9; i++){
+        sudoku[i] = new Array(9);
+    }
+
+    for (var i = 0; i < 9; i++){
+        for (var j = 0; j < 9; j++){
+            const I = String.fromCharCode(48 + i)
+            const J = String.fromCharCode(48 + j)
+            let s=I+J
+            sudoku[i][j] = document.getElementById(s).value;
+        }
+    }
+    var cond = solve(sudoku)
+    
+    if (cond == false)
+        console.alert("Invalid Sudoku")
+}
